0f07c682316d6f76dfa82d6c9fa2cc55d73e2e3d
lixiangAndroid <847250110@qq.com>
2017-11-27 14:04:54 +0800

    利用BuildConfig.DEBUG来控制日志输出

--- app/src/main/java/com/lx/utils/LogUtil.java
+++ app/src/main/java/com/lx/utils/LogUtil.java
@@ -11,72 +11,65 @@ public class LogUtil {
     private LogUtil() {
     }
 
-    public static final String TAG     = "TAG";
-    public static final int    VERBOSE = 1;
-    public static final int    DEBUG   = 2;
-    public static final int    INFO    = 3;
-    public static final int    WARN    = 4;
-    public static final int    ERROR   = 5;
-    public static final int    NOTHING = 6;
-    public static final int    LEVEL   = VERBOSE;//打印所有等级的信息
-//    public static final int    LEVEL   = NOTHING;//关闭所有等级的信息
+    private static final String  TAG   = "TAG";
+    private static final boolean DEBUG = BuildConfig.DEBUG;//利用BuildConfig.DEBUG来控制日志输出
 
     public static void v(String msg) {
-        if (LEVEL <= VERBOSE) {
+        if (DEBUG) {
             Log.v(TAG, msg);
         }
     }
 
     public static void d(String msg) {
-        if (LEVEL <= DEBUG) {
+        if (DEBUG) {
             Log.d(TAG, msg);
         }
     }
 
     public static void i(String msg) {
-        if (LEVEL <= INFO) {
+        if (DEBUG) {
             Log.i(TAG, msg);
         }
     }
 
     public static void w(String msg) {
-        if (LEVEL <= WARN) {
+        if (DEBUG) {
             Log.w(TAG, msg);
         }
     }
 
     public static void e(String msg) {
-        if (LEVEL <= ERROR) {
+        if (DEBUG) {
             Log.e(TAG, msg);
         }
     }
 
     public static void v(String tag, String msg) {
-        if (LEVEL <= VERBOSE) {
+        if (DEBUG) {
             Log.v(tag, msg);
         }
     }
 
     public static void d(String tag, String msg) {
-        if (LEVEL <= DEBUG) {
+        if (DEBUG) {
             Log.d(tag, msg);
         }
     }
 
     public static void i(String tag, String msg) {
-        if (LEVEL <= INFO) {
+        if (DEBUG) {
             Log.i(tag, msg);
         }
     }
 
     public static void w(String tag, String msg) {
-        if (LEVEL <= WARN) {
+        if (DEBUG) {
             Log.w(tag, msg);
         }
     }
 
     public static void e(String tag, String msg) {
-        if (LEVEL <= ERROR) {
+        if (DEBUG) {
             Log.e(tag, msg);
         }
     }
